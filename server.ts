cb0c10e454b43dc4c7558a5ad07130544bbf5988
mongmang <mongmang.vak@gmail.com>
2022-02-09 12:24:27 +0700

    update core

--- server.ts
+++ server.ts
@@ -5,11 +5,24 @@ const cors = require("cors");
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
+import socketIO, { Server } from "socket.io";
+import http from "http";
+import path from "path";
 
 createConnection()
   .then(async (connection) => {
     // create express app
+    const publicPath = path.join(__dirname, "/../public");
     const app = express();
+    let server = http.createServer(app);
+    const io = new Server<
+      ClientToServerEvents,
+      ServerToClientEvents,
+      InterServerEvents,
+      SocketData
+    >();
+
+    app.use(express.static(publicPath));
     app.use(express.json());
     app.use(express.text());
     app.use(express.urlencoded({ extended: false }));
@@ -17,8 +30,27 @@ createConnection()
     app.use(logger);
     // --Route Config ----
     app.use(api_route);
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(` server is listerning on port ${PORT}`);
     });
   })
   .catch((error) => console.log(error));
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
